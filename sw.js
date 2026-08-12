96c24dddacafee4fa9a5ef53ca6bed804f7cfbe4
lalithkumar-rgb <lalithkumargonamanda@gmail.com>
2026-08-12 16:38:15 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'ledger-cache-v1';
+const CACHE = 'ledger-cache-v3';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-180.png', './icon-512.png'];
 
 self.addEventListener('install', e=>{
